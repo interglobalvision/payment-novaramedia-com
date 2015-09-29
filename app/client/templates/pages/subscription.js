655154275b4d4d6bf6a9c82217d3597c05f99e51
Patrick Best <patrickbest@patrickbest.com>
2015-09-29 13:28:31 -0500

    add subscription basic logic & template

---
+++ app/client/templates/pages/subscription.js
@@ -0,0 +1,62 @@
+Template.subscription.onCreated(function () {
+  var _this = this;
+
+});
+
+Template.subscription.onRendered(function () {
+  var _this = this;
+
+});
+
+Template.subscription.events({
+
+  'submit #subscription': function(e, instance) {
+    e.preventDefault();
+
+    var $form = $(e.target);
+    var chargeAmount = $form.find('#donation-amount').val();
+    var email = $form.find('#email').val();
+
+    $form.find('button').prop('disabled', true);
+
+    Stripe.card.createToken($form, function(status, response) {
+
+      if (status === 200) {
+
+        console.log(response);
+        Meteor.call('createSubscription', email, response, chargeAmount, function(err, response) {
+
+          if (err) {
+            console.log(err);
+            alert(err.message);
+          } else {
+            console.log(response);
+
+          }
+
+          $form.find('button').prop('disabled', false);
+
+        });
+
+      } else if (status === 400) {
+
+        // Bad Request	Often missing a required parameter.
+        console.log(response);
+
+        $form.find('button').prop('disabled', false);
+
+
+      } else if (status === 402) {
+
+        // Request Failed	Parameters were valid but request failed.
+        console.log(response);
+
+        $form.find('button').prop('disabled', false);
+
+      }
+
+    });
+
+  }
+
+});
--- app/server/methods/stripeMethods.js
+++ app/server/methods/stripeMethods.js
@@ -1,6 +1,8 @@
 var Stripe = StripeAPI(Meteor.settings.stripe.secret);
 
 var syncChargesCreate = Meteor.wrapAsync(Stripe.charges.create, Stripe.charges);
+var syncCustomersCreate = Meteor.wrapAsync(Stripe.customers.create, Stripe.customers);
+var syncSubscriptionsCreate = Meteor.wrapAsync(Stripe.customers.createSubscription, Stripe.customers);
 
 Meteor.methods({
   singleCharge: function(token, amount) {
@@ -26,6 +28,68 @@ Meteor.methods({
       throw new Meteor.Error('card-charge-failure', 'Sorry but your card charge failed.');
 
     }
+
+  },
+
+  createSubscription: function(email, token, amount) {
+    check(email, String);
+    check(token, Match.ObjectIncluding({id: String,}));
+    amount = parseInt(amount);
+    check(amount, Number);
+
+    console.log('amount', amount);
+
+    // check for existing meteor user
+
+    if (Accounts.findUserByEmail(email)) {
+      throw new Meteor.Error('account-exists', 'You already have an account with this email. To add/change/remove subscriptions please log in first.');
+    }
+
+    // create stripe customer
+
+    try {
+
+      var stripeCustomer = syncCustomersCreate({
+        source: token.id,
+        email: email,
+      });
+
+    } catch(error) {
+      console.log('Stripe customer create error:', error);
+      throw new Meteor.Error('stipe-account-creation-failed', 'Sorry Stripe failed to create a user account.');
+    }
+
+    // create meteor user with stripe customer id saved
+
+    var newUser = Accounts.createUser({email: email, profile: {stripeCustomer: stripeCustomer.id},});
+
+    if (!newUser) {
+      throw new Meteor.Error('account-creation-failed', 'Sorry failed to create a user account.');
+    }
+
+    // create stripe subscription
+
+    try {
+
+      var stripeSubscription = syncSubscriptionsCreate(stripeCustomer.id ,{
+        plan: 'novarasub',
+        quantity: amount,
+      });
+
+    } catch(error) {
+      console.log('Stripe subscription create error:', error);
+      throw new Meteor.Error('stipe-subscription-creation-failed', 'Sorry Stripe failed to create the subscription.');
+    }
+
+    // save stripe subscription to Subscription record
+
+    var newSubscription = Subscriptions.insert({createdAt: new Date(), user: newUser, amount: amount, stripeId: stripeSubscription.id,});
+
+    if (!newSubscription) {
+      throw new Meteor.Error('subscription-creation-failed', 'Sorry failed to create a subscription record.');
+    }
+
+    return true;
 
   }
 });
