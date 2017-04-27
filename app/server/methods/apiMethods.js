bc0d4439f39df4c7a90f07e00bb95e9e67765a32
Patrick Best <patrickbest@patrickbest.com>
2017-04-26 19:20:10 -0500

    apiGoal method using values from setting.json

--- app/server/methods/apiMethods.js
+++ app/server/methods/apiMethods.js
@@ -1,25 +1,34 @@
 Meteor.methods({
 
-  apiTotal: function() {
-    var subscriptions = Subscriptions.find();
+  apiGoal: function() {
+    var start = moment(Meteor.settings.goal.startDate);
+    var end = moment(Meteor.settings.goal.endDate);
+
+    var subscriptions = Subscriptions.find({createdAt: {$gte: start.toDate(), $lt: end.toDate(),},});
     var subscriptionsTotal = 0;
+
+    console.log(subscriptions);
 
     subscriptions.forEach(function (post) {
       subscriptionsTotal += post.amount;
     });
 
-//     console.log('Total in subs', subscriptionsTotal);
-
-    var donations = Donations.find();
+    var donations = Donations.find({createdAt: {$gte: start.toDate(), $lt: end.toDate(),},});
     var donationsTotal = 0;
 
     donations.forEach(function (post) {
       donationsTotal += post.amount;
     });
 
-//     console.log('Total in donations', donationsTotal);
+    var total = subscriptionsTotal + donationsTotal + Meteor.settings.goal.externalDonationsAmount;
+    var percent = total / Meteor.settings.goal.goalAmount;
 
-    return subscriptionsTotal + donationsTotal;
+    return {
+      'total': total,
+      'percent': percent,
+      'timeLeft': end.fromNow(),
+      'timeLeftValue': end.fromNow(true),
+    };
 
   },
 
