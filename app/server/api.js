2ce0f73c5ba8791329e037a157cb0b007878abee
Patrick Best <patrickbest@patrickbest.com>
2017-04-26 19:19:50 -0500

    conditionally enable endpoint and change url

--- app/server/api.js
+++ app/server/api.js
@@ -1,9 +1,13 @@
-Router.route('/api/total', function(){
-  this.response.statusCode = 200;
-  this.response.setHeader("Content-Type", "application/json");
-  this.response.setHeader("Access-Control-Allow-Origin", "*");
-  this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  this.response.end(JSON.stringify(
-    Meteor.call('apiTotal')
-  ));
-}, {where: 'server',});
+if (Meteor.settings.goal.enableApi === true) {
+
+  Router.route('/api/goal', function(){
+    this.response.statusCode = 200;
+    this.response.setHeader("Content-Type", "application/json");
+    this.response.setHeader("Access-Control-Allow-Origin", "*");
+    this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    this.response.end(JSON.stringify(
+      Meteor.call('apiGoal')
+    ));
+  }, {where: 'server',});
+
+}
