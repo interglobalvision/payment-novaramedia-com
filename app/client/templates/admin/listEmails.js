d6ba583680b5471a49c2f7c1d40087828c273f71
Patrick Best <patrickbest@patrickbest.com>
2017-04-28 19:45:36 -0500

    output subs, non subs and all users lists
    with select on click

--- app/client/templates/admin/listEmails.js
+++ app/client/templates/admin/listEmails.js
@@ -1,7 +1,54 @@
+Template.listEmails.onCreated(function() {
+//   console.log(this);
+});
+
+Template.listEmails.events({
+  'click .email-export' (event) {
+//     console.log(event);
+    selectText($(event.currentTarget));
+  },
+});
+
 Template.listEmails.helpers({
   displayEmail: function(userId) {
     var user = Meteor.users.findOne(userId);
 
     return user.emails[0].address;
   },
+
+  expiredUsers: function () {
+
+    var expiredUsers = Meteor.users.find({}).fetch().filter(function (doc) {
+      var userSubscription = Subscriptions.findOne({user: doc._id});
+
+      if (userSubscription) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    var expiredUsersIds = _.pluck(expiredUsers, '_id');
+
+    return Meteor.users.find({_id: {$in: expiredUsersIds}});
+  },
 });
+
+function selectText($item) {
+  var doc = document;
+  var element = $item[0];
+  var range;
+  var selection;
+
+  if (doc.body.createTextRange) {
+    range = document.body.createTextRange();
+    range.moveToElementText(element);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection();
+    range = document.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+}
