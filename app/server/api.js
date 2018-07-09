476275f40a8c8739efd6a897e8dfddfa4a31d864
Cas <cas@interglobal.vision>
2018-07-09 15:12:13 -0500

    add webhook endpoint

--- app/server/api.js
+++ app/server/api.js
@@ -11,3 +11,17 @@ if (Meteor.settings.fundraiser.enable === true) {
   }, {where: 'server',});
 
 }
+
+Router.route('/api/stripeWebhook', function(event){
+  const { body } = event;
+
+  console.log('WEBHOOK INVOKED');
+
+  this.response.statusCode = 200;
+  this.response.setHeader("Content-Type", "application/json");
+  this.response.setHeader("Access-Control-Allow-Origin", "*");
+  this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  this.response.end(JSON.stringify({})); // Send a response back to Stripe. This info can be seen in the Events history
+
+  // DO SOMETHING HERE
+}, {where: 'server',});
