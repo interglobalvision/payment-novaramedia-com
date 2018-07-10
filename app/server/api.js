d9d27ba5d8886c349fc02b6f93b3ae89912d30f2
varffs <patrickbest@Patricks-MacBook-Pro.local>
2018-07-10 21:51:23 +0100

    add code comments and scaffold of switch

--- app/server/api.js
+++ app/server/api.js
@@ -2,9 +2,9 @@ if (Meteor.settings.fundraiser.enable === true) {
 
   Router.route('/api/goal', function(){
     this.response.statusCode = 200;
-    this.response.setHeader("Content-Type", "application/json");
-    this.response.setHeader("Access-Control-Allow-Origin", "*");
-    this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    this.response.setHeader('Content-Type', 'application/json');
+    this.response.setHeader('Access-Control-Allow-Origin', '*');
+    this.response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     this.response.end(JSON.stringify(
       Meteor.call('apiGoal')
     ));
@@ -12,16 +12,36 @@ if (Meteor.settings.fundraiser.enable === true) {
 
 }
 
-Router.route('/api/stripeWebhook', function(event){
-  const { body } = event;
+Router.route('/api/stripeWebhook', function(event) {
+  const { headers, body } = event;
+
+  //>>> Logging for dev
 
   console.log('WEBHOOK INVOKED');
+  console.log('headers', headers);
+  console.log('body', body);
+
+  //>>> Return 200 to Stripe to acnoledge reciept
 
   this.response.statusCode = 200;
-  this.response.setHeader("Content-Type", "application/json");
-  this.response.setHeader("Access-Control-Allow-Origin", "*");
-  this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  this.response.end(JSON.stringify({})); // Send a response back to Stripe. This info can be seen in the Events history
+  this.response.setHeader('Content-Type', 'application/json');
+  this.response.setHeader('Access-Control-Allow-Origin', '*');
+  this.response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  this.response.end(); // Send a response back to Stripe. This info can be seen in the Events history
 
-  // DO SOMETHING HERE
+  //>>> TODO: Call stripe.webhooks.constructEvent with body, headers.stripe-signature & Meteor.settings.stripe.endpointSecret to verify event is from Stripe. Use returned event as data for switch below
+
+  //>>> Switch event type for different handling
+
+  switch(body.type) {
+    case 'customer.subscription.updated':
+      console.log('sub updated');
+      break;
+    case 'customer.subscription.deleted':
+      console.log('sub deleted');
+      break;
+    default:
+      return;
+  }
+
 }, {where: 'server',});
