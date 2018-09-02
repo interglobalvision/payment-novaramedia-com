7dfb1daf1ce8c15e558f5f6097365dd7c69cc50d
varffs <patrickbest@Patricks-MacBook-Pro.local>
2018-09-02 15:57:18 +0100

    add counts to headings
    and rationalize handlebars helpers

--- app/client/helpers/spacebars.js
+++ app/client/helpers/spacebars.js
@@ -6,8 +6,19 @@ Custom Handlebars helpers.
 
 /+ ---------------------------------------------------- */
 
+// Returns a from now string from a data object
 Handlebars.registerHelper('timeSince', function(date) {
-
   return moment(date).fromNow();
+});
 
+// Returns the count of a collection
+Handlebars.registerHelper('displayCount', function(cursor) {
+  return cursor.count();
+});
+
+// Returns the string of the first email associated with a user account
+Handlebars.registerHelper('displayEmail', function(userId) {
+  var user = Meteor.users.findOne(userId);
+
+  return user.emails[0].address;
 });
